425e282fae982470cd057aac5a48fae0cc6f84aa
rumeysa111 <rumeysasemiz2003@gmail.com>
2024-12-19 21:49:50 +0300

    project files added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
+const Document = require('./models/document');
 const app = express();
 const jwt=require('jsonwebtoken');
 const cors = require('cors');
+const http = require('http');
 const documentRouter = require('./routes/document');
-
+var server=http.createServer(app);
 const DB = "mongodb+srv://rumeysasmz:2003Rumeysa@cluster0.r1xyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+var io = require('socket.io')(server);
 // express.json() middleware'ini kullanarak gelen isteklerin gövdesini JSON formatında ayrıştır
 
 app.use(express.json());
@@ -21,10 +23,42 @@ mongoose.connect(DB).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => console.log(err));
 
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);  // Socket ID ile bağlantıyı kontrol edin.
+
+    // Diğer olaylar
+    socket.on('join', (documentId) => {
+        socket.join(documentId);
+        console.log('User joined document:', documentId);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.room).emit('changes', data);
+    });
+
+    socket.on('save', (data) => {
+        saveData(data);
+    });
+});
+
+const saveData = async (data) => {
+    let document = await Document.findById(data.room);
+    if (!document) {
+        console.log("Document not found");
+        return;
+    }
+    document.data = data.delta;
+    document = await document.save();
+}
+
 //async _> await
 // .then(data) =>printdata
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
  
 
--- server/routes/document.js
+++ server/routes/document.js
@@ -23,12 +23,33 @@ documentRouter.post('/doc/create', auth, async (req, res) => {
     }
 
 });
-documentRouter.get('/docs/me', auth, async (req, res) => {
+documentRouter.get('/docs/:id', auth, async (req, res) => {
     try {
-        let documents = await Document.find({ uid: req.user });
+        const  documents = await Document.findById(req.params.id );
         res.json(documents);
         
         
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        
+    }
+});
+documentRouter.get("/docs/me", auth, async (req, res) => {
+    try {
+      let documents = await Document.find({ uid: req.user });
+      res.json(documents);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
+
+//belge başlıgı güncelleme 
+documentRouter.post("/doc/title", auth, async (req, res) => {
+    try {
+        const { id, title } = req.body;
+        const document = await Document.findByIdAndUpdate(id, { title });
+        res.json(document);
     } catch (error) {
         res.status(500).json({ error: error.message });
         
